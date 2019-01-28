77743b58b24d634b141bc2f4167290822aab37eb
Pramod Tiwari <tpromode@gmail.com>
2019-01-28 11:34:38 +0545

    update local setting sample

---
+++ office/office/local_settings_sample.py
@@ -0,0 +1,42 @@
+from .settings import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'office',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        }
+}
+
+# DEBUG = False
+# INSTALLED_APPS += [
+
+#     'debug_toolbar',
+
+# ]
+
+# MIDDLEWARE += [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+# ]
+# INTERNAL_IPS = '127.0.0.1'
+
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static")
+    ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'a-static')
+
+
+# WEBPACK_LOADER = {
+#     'DEFAULT': {
+#         'CACHE': False,
+#         # 'BUNDLE_DIR_NAME': '/dist/',#('/build/' if DEBUG else '/dist/'),
+#         'BUNDLE_DIR_NAME': '/build/',#('/build/' if DEBUG else '/dist/'),
+#         # 'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+#         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-local.json'),
+#     }
+# }
